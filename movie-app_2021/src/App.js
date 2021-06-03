b9a061ebd2117920f25fc531db4adba1bb8e6791
leejuhyeon <esloven3@gmail.com>
2021-06-03 12:34:22 +0900

    Rendering the Movies_1

--- movie-app_2021/src/App.js
+++ movie-app_2021/src/App.js
@@ -1,38 +1,62 @@
 import React from "react";
-import PT from "prop-types";
 import axios from "axios";
+import Movie from "./Movie";
 
-class App extends React.Component{
-
+class App extends React.Component {
   state = {
-    isLoading : true,
-    movies : []
+    isLoading: true,
+    movies: [],
   };
 
   // You must waiting to do "await".
-  getMovies = async () => {
-    var movies = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json");
+
+  // axios
+  // fetch 위에 있는 작은 layor 같다.
+  // 설치 명령어 : npm i axios
+
+  _getMovies = async () => {
+    var {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      "https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating"
+    );
+    console.log(movies);
+    this.setState({ movies, isLoading: false });
+  };
+  get getMovies() {
+    return this._getMovies;
+  }
+  set getMovies(value) {
+    this._getMovies = value;
   }
 
-  componentDidMount(){
-    
-  this.getMovies();
-
-    // axios
-    // fetch 위에 있는 작은 layor 같다.
-    // 설치 명령어 : npm i axios
-
+  componentDidMount() {
+    this.getMovies();
   }
 
-  render(){
-    var {isLoading} = this.state;
+  render() {
+    var { isLoading, movies } = this.state;
     return (
-    <div>
-      {isLoading ? "Loading..." : "We are ready!"}
-    </div>
+      <div>
+        {isLoading
+          ? "Loading..."
+          : movies.map((movie) => {
+              return (
+                <Movie
+                  key={movie.id}
+                  id={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+                />
+              );
+            })}
+      </div>
     );
   }
-
 }
 
 export default App;
