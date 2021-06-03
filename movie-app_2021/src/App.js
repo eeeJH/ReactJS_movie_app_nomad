81146417d0ffa8d31fdc747a09e27ce38e0aae24
Juhyeon Lee <juhyeonlee@Juhyeons-MacBook-Pro.local>
2021-06-03 09:08:39 +0900

    Planning the Movie Component

--- movie-app_2021/src/App.js
+++ movie-app_2021/src/App.js
@@ -3,45 +3,21 @@ import PT from 'prop-types';
 
 class App extends React.Component{
 
-  constructor(props){
-    super(props);
-    console.log("Hello");
-  }
-
   state = {
-    count: 0
+    isLoading : true
   };
-
-  plus = () => {
-    console.log("plus");
-    this.setState(current => ({count : this.state.count + 1}));
-  };
-  minus = () => {
-    console.log("minus");
-    this.setState(current => ({count : this.state.count - 1}));
-  };
-
-  // setState를 호출할때마다 will rerender.
 
   componentDidMount(){
-    console.log("componentDidMount!");
-  }
-
-  componentDidUpdate(){
-    console.log("componentDidUpdate");
-  }
-
-  // ?? 페이지에서 not disappear.
-  componentWillUnmount(){
-    console.log("componentWillUnmount");
+    setTimeout(() => {
+      this.setState({isLoading : false});
+    }, 5000);
   }
 
   render(){
+    var {isLoading} = this.state;
     return (
     <div>
-      <h1>The number is {this.state.count}</h1>
-      <button onClick={this.plus}>Plus</button>
-      <button onClick={this.minus}>Minus</button>
+      {isLoading ? "Loading..." : "We are ready!"}
     </div>
     );
   }
